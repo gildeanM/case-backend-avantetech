8611b2f55c80cbd36fd784719a98bda012e98cd1
Gildean Morais <113740969+gildeanM@users.noreply.github.com>
2025-11-06 17:43:18 +0000

    feat: add cors

--- src/index.js
+++ src/index.js
@@ -2,22 +2,27 @@ const express = require("express");
 const swaggerUi = require("swagger-ui-express");
 const swaggerSpec = require("./docs/swagger");
 const errorHandling = require("./middleware/error-handling");
+const cors = require("cors");
 require("dotenv").config();
 require("express-async-error");
 
-const PORT = process.env.PORT || 3000;
 const app = express();
 const categoryRoutes = require("./routes/category-routes");
 const productsRoutes = require("./routes/products-routes");
 
+app.use(cors());
 app.use(express.json());
 
+app.get("/", (req, res) => {
+    res.send("Welcome to the Avanté Tech Backend Challenge API!");
+});
 app.use("/", categoryRoutes);
 app.use("/", productsRoutes);
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 app.use(errorHandling);
 
 
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
 })
