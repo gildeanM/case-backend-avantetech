0f39dd364c1ed79e18646087f3e84d1616205801
Gildean Morais <113740969+gildeanM@users.noreply.github.com>
2025-11-06 18:08:18 +0000

    fix: message of server running

--- src/index.js
+++ src/index.js
@@ -24,6 +24,6 @@ app.use(errorHandling);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
+    console.log(`Server is running on https://case-backend-avantetech.onrender.com`);
 })
 
